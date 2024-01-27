ba17c0e0fb76fe36b6f4eddc7c50915c6928ddbf
Niall Lewin <Lewin91@hotmail.com>
2024-01-27 01:04:42 +0000

    Added weird height thing

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,9 +1,15 @@
+import { useEffect, useState } from "react";
 import styles from "./hero.module.scss";
 
 const Hero = ({ imgSrc }: { imgSrc: string }) => {
+  const [height, setHeight] = useState(400);
+
+  useEffect(() => {
+    setHeight(window.innerWidth * 0.37);
+  }, [])
 
   return (
-    <div className={styles.heroContainer}>
+    <div className={styles.heroContainer} style={{ height }}>
       <img src={imgSrc} alt="Cute cat" />
     </div>
   );
